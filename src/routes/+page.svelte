8fc8c2ae4466dce99117698fb4a0c15f97d32856
DAWSLJ1 <DAWSLJ1@student.op.ac.nz>
2026-09-25 10:52:59 +1200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+import bill from ''
+</script>
+
+<header>
+<h1>We Learn Svelte</h1>
+<nav>
+  <ul>
+    <li><a href="/">Home</a></li>
+  <li><a href="/about">About</a></li>
+  <li><a href="/contact">Contact Us</a></li>
+</nav>
+</header>
+
+
+<main>
+  <article>
+  <figure>
+  <img src="" alt="">
+  <figcaption>@username</figcaption>
+  </figure>
+  <section>
+  <h2>Boring cards</h2>
+  <ul>
+    <li>One</li>
+    <li>Two</li>
+    <li>Three</li>
+  </ul>
+  </section>
+</article>
+
+</main>
+
+<footer><p>&copy; Fundamentals of Web</p></footer>
+<script>
+<article>{
+  border: 1px solid grey;
+}
+
+</script>
